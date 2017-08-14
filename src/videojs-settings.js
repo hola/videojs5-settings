a373817e2b2a941bcc6a98cfcea7f411b11fd5a6
Andrey Kashta <andrey@hola.org>
2017-08-14 15:32:25 +0300

    change settings icon

--- src/videojs-settings.js
+++ src/videojs-settings.js
@@ -3,11 +3,22 @@
 require('@hola.org/videojs-utils');
 var Clipboard = require('clipboard');
 var find = require('lodash/find');
-var settings_icon_svg = '<svg height="3.6em" width="3.6em" viewBox="-10 -10 36 36">'
-        +'<path class="st0" d="M16,9.4V6.6h-2.2c-0.2-0.6-0.4-1.3-0.8-1.8l1.5-1.5l-1.9-1.9l-1.5,1.5C10.6,2.6,10,2.3,9.4,2.2V0H6.6v2.2'
-        +'C6,2.3,5.4,2.6,4.8,2.9L3.3,1.4L1.4,3.3l1.5,1.5C2.6,5.4,2.3,6,2.2,6.6H0v2.7h2.2c0.2,0.6,0.4,1.3,0.8,1.8l-1.5,1.5l1.9,1.9l1.5-1.5'
-        +'c0.5,0.3,1.2,0.6,1.8,0.8V16h2.7v-2.2c0.6-0.2,1.3-0.4,1.8-0.8l1.5,1.5l1.9-1.9l-1.5-1.5c0.3-0.5,0.6-1.2,0.8-1.8H16z M8,11'
-        +'c-1.7,0-3-1.3-3-3s1.3-3,3-3s3,1.3,3,3S9.7,11,8,11z"/>'
+var settings_icon_svg = '<svg viewBox="0 0 36 36">'
+    +'<path class="st0" d="m 23.94,18.78 c .03,-0.25 .05,-0.51 .05,-0.78 0,'
+    +'-0.27 -0.02,-0.52 -0.05,-0.78 l 1.68,-1.32 c .15,-0.12 .19,-0.33 .09,'
+    +'-0.51 l -1.6,-2.76 c -0.09,-0.17 -0.31,-0.24 -0.48,-0.17 l -1.99,.8 c '
+    +'-0.41,-0.32 -0.86,-0.58 -1.35,-0.78 l -0.30,-2.12 c -0.02,-0.19 -0.19,'
+    +'-0.33 -0.39,-0.33 l -3.2,0 c -0.2,0 -0.36,.14 -0.39,.33 l -0.30,2.12 c '
+    +'-0.48,.2 -0.93,.47 -1.35,.78 l -1.99,-0.8 c -0.18,-0.07 -0.39,0 -0.48,'
+    +'.17 l -1.6,2.76 c -0.10,.17 -0.05,.39 .09,.51 l 1.68,1.32 c -0.03,.25 '
+    +'-0.05,.52 -0.05,.78 0,.26 .02,.52 .05,.78 l -1.68,1.32 c -0.15,.12 '
+    +'-0.19,.33 -0.09,.51 l 1.6,2.76 c .09,.17 .31,.24 .48,.17 l 1.99,-0.8 c '
+    +'.41,.32 .86,.58 1.35,.78 l .30,2.12 c .02,.19 .19,.33 .39,.33 l 3.2,0 c '
+    +'.2,0 .36,-0.14 .39,-0.33 l .30,-2.12 c .48,-0.2 .93,-0.47 1.35,-0.78 l '
+    +'1.99,.8 c .18,.07 .39,0 .48,-0.17 l 1.6,-2.76 c .09,-0.17 .05,-0.39 '
+    +'-0.09,-0.51 l -1.68,-1.32 0,0 z m -5.94,2.01 c -1.54,0 -2.8,-1.25 -2.8,'
+    +'-2.8 0,-1.54 1.25,-2.8 2.8,-2.8 1.54,0 2.8,1.25 2.8,2.8 0,1.54 -1.25,'
+    +'2.8 -2.8,2.8 l 0,0 z"/>'
     +'</svg>';
 function extend_component(name, parent, comp){
     vjs.registerComponent(name, vjs.extend(vjs.getComponent(parent), comp));
@@ -490,13 +501,6 @@ extend_component('SettingsButton', 'MenuButton', {
         this.player_.toggleClass('vjs-settings-expanded', this.buttonPressed_);
         this.el_.setAttribute('aria-expanded', this.buttonPressed_);
         this.menu.show(this.buttonPressed_);
-        var svg = this.el_.querySelector('.vjs-button-icon>svg>path');
-        if (svg)
-        {
-            // IE and Edge don't support svg css transform
-            svg.setAttribute('transform', this.buttonPressed_ ?
-                'rotate(-30, 8, 8)' : '');
-        }
     },
     unpressButton: function(){
         if (!this.enabled_)
